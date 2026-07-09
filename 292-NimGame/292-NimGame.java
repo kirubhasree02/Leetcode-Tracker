f78f907a90e0fb9ba83afa2cba616b9181057624
kirubhasree02 <kirubhasreeganesan28@gmail.com>
2026-07-09 15:10:43 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 09/07/2026, 15:10:42
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
