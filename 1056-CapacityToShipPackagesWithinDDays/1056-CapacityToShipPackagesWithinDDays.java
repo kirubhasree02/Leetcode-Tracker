dbba673ea5b54edbb53527459db4e1b2ef0fd216
kirubhasree02 <kirubhasreeganesan28@gmail.com>
2026-07-09 15:08:28 +0530

    Create 1056-CapacityToShipPackagesWithinDDays

---
+++ 1056-CapacityToShipPackagesWithinDDays/1056-CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 15:08:27
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int left=0,right=0;
+        for(int w:weights){
+            left=Math.max(left,w);
+            right+=w;
+        }
+        while(left<right){
+            int mid=(left+right)/2,need=1,cur=0;
+            for(int w:weights){
+                if(cur+w>mid){
+                    need+=1;
+                    cur=0;
+                }
+                cur+=w;
+            }
+            if(need>days) left=mid+1;
+            else right=mid;
+        }
+        return left;
+    }
+}
