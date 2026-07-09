76e2bacd723c75f79239e1a258813d5d67c5bbac
kirubhasree02 <kirubhasreeganesan28@gmail.com>
2026-07-09 15:11:28 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 15:11:27
+class Solution {
+    public boolean isHappy(int n) {
+        if(n==1||n==7) return true;
+        else if(n<10) return false;
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum+=d*d;
+            n/=10;
+        }
+        return isHappy(sum);
+       
+    }
+}
