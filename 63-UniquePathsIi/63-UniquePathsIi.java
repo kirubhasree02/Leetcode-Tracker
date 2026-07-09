81cb6823d9486d77720c4c4aaaac4401d04f4415
kirubhasree02 <kirubhasreeganesan28@gmail.com>
2026-07-09 15:13:50 +0530

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.java
@@ -0,0 +1,17 @@
+// Last updated: 09/07/2026, 15:13:49
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int width = obstacleGrid[0].length;
+    int[] dp = new int[width];
+    dp[0] = 1;
+    for (int[] row : obstacleGrid) {
+        for (int j = 0; j < width; j++) {
+            if (row[j] == 1)
+                dp[j] = 0;
+            else if (j > 0)
+                dp[j] += dp[j - 1];
+        }
+    }
+    return dp[width - 1];
+   }
+}
