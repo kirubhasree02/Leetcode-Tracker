a254c8d75a9325727d8d392f0179ca7e2e5d8bae
kirubhasree02 <kirubhasreeganesan28@gmail.com>
2026-07-09 15:08:31 +0530

    Create 1031-AddToArrayFormOfInteger

---
+++ 1031-AddToArrayFormOfInteger/1031-AddToArrayFormOfInteger.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 15:08:30
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> res=new LinkedList<>();
+        for(int i=num.length-1;i>=0;i--){
+            res.add(0,(num[i]+k)%10);
+            k=(num[i]+k)/10;
+        }
+        while(k>0){
+            res.add(0,k%10);
+            k/=10;
+        }
+        return res;
+    }
+}
